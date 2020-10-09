88719bbc8bc537b105ca6dbe0e725b1f7507f520
Gesenia <gesenia123@gmail.com>
2020-10-08 22:15:48 -0500

    a

--- JS/app5.js
+++ JS/app5.js
@@ -1,7 +1,7 @@
 //AREA CIRCUNFERENCIA
 
-let r = parseInt(prompt("Ingresar radio de circunferencia"));
-p=3.1416;
+let r = prompt("Ingresar radio de circunferencia");
+let p = 3.1416;
 
-let total = (p*r^2);
+let total = (p * (r * r));
     alert(total);
---
+++ JS/app6.js
@@ -0,0 +1,10 @@
+//corregir
+
+let number1 = prompt("Ingresar Horas Trabajadas por semana");
+let number2 = prompt("Ingresar Sueldo por Hora");
+
+let total = (number1 * number2)
+
+
+//buscar otra manera
+
